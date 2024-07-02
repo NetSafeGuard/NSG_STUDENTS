2265db1ec6eceb4f096a21d63fd630740411fecf
Z4dEV <z4kreyyt@gmail.com>
2024-07-02 14:34:26 +0100

    fix: Fixing wallpaper path

--- src/pages/App/style.ts
+++ src/pages/App/style.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import bg from './assets/wallpaper.png';
+import bg from '../../assets/wallpaper.png';
 
 export const Container = styled.div`
     display: flex;
