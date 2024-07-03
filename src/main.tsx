df54d3383e183c50cf061a71d77b8fb31bc14ca6
Z4dEV <z4kreyyt@gmail.com>
2024-07-03 09:08:10 +0100

    fix:Adding BrowerRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import './global.css';
 import { TitleBar } from './components/titlebar';
 import { Toaster } from '@/components/ui/sonner';
 import { RoutesList } from './router/routes';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<>
 		<TitleBar />
 		<Toaster />
-		<RoutesList />
+		<BrowserRouter>
+			<RoutesList />
+		</BrowserRouter>
 	</>,
 );
